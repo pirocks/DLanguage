076ef30b6e7bd59e9e6d4bb6759c6df29949a9b3
Francis Nixon <13nixonf@gmail.com>
2018-07-05 16:16:43 -0700

    initial skeleton for debugger tests.

---
+++ src/test/kotlin/io/github/intellij/dlanguage/debugger/DebuggerTest.kt
@@ -0,0 +1,29 @@
+package io.github.intellij.dlanguage.debugger
+
+import com.intellij.openapi.module.Module
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiFile
+import com.intellij.testFramework.fixtures.HeavyIdeaTestFixture
+
+class DebuggerTest : HeavyIdeaTestFixture{
+    override fun getModule(): Module {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun addFileToProject(rootPath: String, relativePath: String, fileText: String): PsiFile {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun tearDown() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getProject(): Project {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun setUp() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
