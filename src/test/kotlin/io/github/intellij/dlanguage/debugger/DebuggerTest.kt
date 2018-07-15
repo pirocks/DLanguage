f3904a90e21a42a431cd42cf10d7b8b5a0776ff6
Francis Nixon <13nixonf@gmail.com>
2018-07-15 11:52:08 -0700

    initial, not compiling,setup code.

--- src/test/kotlin/io/github/intellij/dlanguage/debugger/DebuggerTest.kt
+++ src/test/kotlin/io/github/intellij/dlanguage/debugger/DebuggerTest.kt
@@ -1,29 +1,55 @@
 package io.github.intellij.dlanguage.debugger
 
+import com.intellij.execution.DefaultExecutionResult
+import com.intellij.execution.ExecutionException
+import com.intellij.execution.ExecutionResult
+import com.intellij.execution.runners.ExecutionEnvironment
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiFile
+import com.intellij.testFramework.LightPlatformTestCase
 import com.intellij.testFramework.fixtures.HeavyIdeaTestFixture
+import com.intellij.xdebugger.XDebugProcess
+import com.intellij.xdebugger.XDebugProcessStarter
+import com.intellij.xdebugger.XDebugSession
+import com.intellij.xdebugger.XDebuggerManager
+import io.github.intellij.dlanguage.run.RunUtil
+import uk.co.cwspencer.gdb.Gdb
+import uk.co.cwspencer.gdb.messages.GdbEvent
+import uk.co.cwspencer.ideagdb.debug.GdbDebugProcess
 
-class DebuggerTest : HeavyIdeaTestFixture{
-    override fun getModule(): Module {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun addFileToProject(rootPath: String, relativePath: String, fileText: String): PsiFile {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun tearDown() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun getProject(): Project {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
+class DebuggerTest : LightPlatformTestCase(){
     override fun setUp() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+        val project = getProject()!!;
 
+        val executableFilePath = "";//todo
+        val env:ExecutionEnvironment = ExecutionEnvironment();
+        val result: ExecutionResult = DefaultExecutionResult();
+        val debugSession = XDebuggerManager.getInstance(project).startSession(env,
+            object : XDebugProcessStarter() {
+                @Throws(ExecutionException::class)
+                override fun start(session: XDebugSession): XDebugProcess {
+                    return GdbDebugProcess(project, session, result, executableFilePath)
+                }
+            })
+
+        val debugProcess = debugSession.debugProcess as GdbDebugProcess
+
+        val gdbProcess = debugProcess.m_gdb
+
+        // Queue startup commands
+        gdbProcess.sendCommand("-list-features") { event -> gdbProcess.onGdbCapabilitiesReady(event) }
+
+        // Send startup commands
+        val commandsArray = arrayOfNulls<String>(0)//configuration.STARTUP_COMMANDS.split("\\r?\\n");
+        for (command in commandsArray) {
+            command = command.trim { it <= ' ' }
+            if (!command.isEmpty()) {
+                gdbProcess.sendCommand(command)
+            }
+        }
+
+//        if (configuration.autoStartGdb) {
+        gdbProcess.sendCommand("-exec-run")
+    }
 }
